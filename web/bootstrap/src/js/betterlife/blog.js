da9a3b7fb6cc755d4f67f0e58826ba46c12d1432
skygreen <skygreen2001@itasktour.com>
2017-05-29 08:51:15 +0800

    filter

--- web/bootstrap/src/js/betterlife/blog.js
+++ web/bootstrap/src/js/betterlife/blog.js
@@ -7,6 +7,7 @@ $(function(){
             "language"  : $.dataTable.chinese,
             "processing": true,
             "serverSide": true,
+            "retrieve"  : true,
             "ajax": {
                 "url" : "../../data/list.json",
                 "data": function ( d ) {
@@ -28,8 +29,7 @@ $(function(){
                     var json = jQuery.parseJSON( data );
                     var p = infoTable.ajax.params();
                     json.draw = p.draw;
-                    var info = infoTable.page.info();
-                    if ((info.end-info.start) >= info.length){
+                    if ((p.start + p.length) <= json.recordsFiltered){
                         json.data = json.data.slice(p.start, p.length);
                     }else{
                         json.data = json.data.slice(p.start);
@@ -56,66 +56,66 @@ $(function(){
                 { data:"id" }
             ],
             "columnDefs": [
-                {"orderable":false,"targets":5,
-                    "render" : function(data, type, row) {
-                        var data     = {
-                            "img_id"  : "imgUrl"+row.id,
-                            "img_src" : data,
-                            "img_name": row.imgName
-                        };
-                        var result = $.templates("#imgTmpl").render(data);
+                {"orderable": false, "targets": 5,
+                 "render"   : function(data, type, row) {
+                    var data = {
+                        "img_id"  : "imgUrl"+row.id,
+                        "img_src" : data,
+                        "img_name": row.imgName
+                    };
+                    var result = $.templates("#imgTmpl").render(data);
 
-                        $("body").off('click', 'a#imgUrl'+row.id);
-                        $("body").on('click', 'a#imgUrl'+row.id, function(){
-                            $('#imagePreview').attr('src', $('a#imgUrl' + row.id + " img").attr('src'));
-                            $('#imagePreview-link').attr('href', $('a#imgUrl' + row.id + " img").attr('src'));
-                            $('#imageModal').modal('show');
-                        });
-                        return result;
-                    }
+                    $("body").off('click', 'a#imgUrl'+row.id);
+                    $("body").on('click', 'a#imgUrl'+row.id, function(){
+                        $('#imagePreview').attr('src', $('a#imgUrl' + row.id + " img").attr('src'));
+                        $('#imagePreview-link').attr('href', $('a#imgUrl' + row.id + " img").attr('src'));
+                        $('#imageModal').modal('show');
+                    });
+                    return result;
+                 }
                 },
-                {"orderable":false, "targets":6,
-                    "render" : function(data,type,row){
-                        if (data==0){
-                            return '<span class="status-fail">审核失败</span>';
-                        }else if ( data == 1 )
-                            return '<span class="status-pass">审核通过</span>';
-                        else{
-                            return '<span class="status-wait">待审核</span>';
-                        }
+                {"orderable": false, "targets": 6,
+                 "render"   : function(data,type,row){
+                    if ( data == 0 ){
+                        return '<span class="status-fail">审核失败</span>';
+                    } else if ( data == 1 ) {
+                        return '<span class="status-pass">审核通过</span>';
+                    } else {
+                        return '<span class="status-wait">待审核</span>';
                     }
+                 }
                 },
-                {"orderable":false, "targets":7,
-                    "render" : function(data, type, row){
-                        var result = $.templates("#actionTmpl").render({ "id"  : data });
+                {"orderable": false, "targets": 7,
+                 "render"   : function(data, type, row){
+                    var result = $.templates("#actionTmpl").render({ "id"  : data });
 
-                        $("body").off('click', 'a#info-view'+data);
-                        $("body").on('click', 'a#info-view'+data, function(){//查看
-                            bootbox.confirm("确定要查看该博客:" + data + "?",function(result){
-                                if ( result == true ){
-                                    console.log("查看博客:" + data);
-                                }
-                            });
+                    $("body").off('click', 'a#info-view'+data);
+                    $("body").on('click', 'a#info-view'+data, function(){//查看
+                        bootbox.confirm("确定要查看该博客:" + data + "?",function(result){
+                            if ( result == true ){
+                                console.log("查看博客:" + data);
+                            }
                         });
-                        $("body").off('click', 'a#info-edit'+data);
-                        $("body").on('click', 'a#info-edit'+data, function(){//修改
-                            bootbox.confirm("确定要编辑该博客:" + data + "?",function(result){
-                                if ( result == true ){
-                                    console.log("编辑博客:" + data);
-                                }
-                            });
+                    });
+                    $("body").off('click', 'a#info-edit'+data);
+                    $("body").on('click', 'a#info-edit'+data, function(){//修改
+                        bootbox.confirm("确定要编辑该博客:" + data + "?",function(result){
+                            if ( result == true ){
+                                console.log("编辑博客:" + data);
+                            }
                         });
-                        $("body").off('click', 'a#info-dele'+data);
-                        $("body").on('click', 'a#info-dele'+data, function(){//删除
-                            bootbox.confirm("确定要删除该博客:" + data + "?",function(result){
-                                if ( result == true ){
-                                    console.log("删除博客:" + data);
-                                }
-                            });
+                    });
+                    $("body").off('click', 'a#info-dele'+data);
+                    $("body").on('click', 'a#info-dele'+data, function(){//删除
+                        bootbox.confirm("确定要删除该博客:" + data + "?",function(result){
+                            if ( result == true ){
+                                console.log("删除博客:" + data);
+                            }
                         });
-                        return result;
-                    }
+                    });
+                    return result;
                 }
+             }
             ],
             "initComplete":function(){ },
             "drawCallback": function( settings ) {
