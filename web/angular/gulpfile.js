051c31860251a053eefca2b877526baef6ac32c0
skygreen2001 <skygreen2001@gmail.com>
2017-10-22 22:15:23 +0800

    refactor gulp

--- web/angular/gulpfile.js
+++ web/angular/gulpfile.js
@@ -46,29 +46,13 @@ if (require('fs').existsSync('./config.js')) {
 =            Requiring stuffs            =
 ========================================*/
 
-var gulp           = require('gulp'),
-
-    bower          = require('gulp-bower'),
-
-    seq            = require('run-sequence'),
-    connect        = require('gulp-connect'),
-    less           = require('gulp-less'),
-    uglify         = require('gulp-uglify'),
-    sourcemaps     = require('gulp-sourcemaps'),
-    cssmin         = require('gulp-cssmin'),
-    // image          = require('gulp-image'),
-    order          = require('gulp-order'),
-    concat         = require('gulp-concat'),
-    ignore         = require('gulp-ignore'),
-    clean          = require('gulp-clean'),
-    templateCache  = require('gulp-angular-templatecache'),
-    mobilizer      = require('gulp-mobilizer'),
-    ngAnnotate     = require('gulp-ng-annotate'),
-    replace        = require('gulp-replace'),
-    ngFilesort     = require('gulp-angular-filesort'),
-    streamqueue    = require('streamqueue'),
-    rename         = require('gulp-rename'),
-    path           = require('path');
+var gulp = require('gulp'),
+    $    = require('gulp-load-plugins')(),
+    path = require('path'),
+    seq  = require('run-sequence'),
+    strip= require('gulp-strip-comments'),
+    streamqueue = require('streamqueue'),
+    bowerFiles  = require('main-bower-files');
 
 /*================================================
 =            Report Errors to Console            =
@@ -84,7 +68,7 @@ gulp.on('error', function(e) {
 =========================================*/
 
 gulp.task('bower', function (cb) {
-  return bower({});
+  return $.bower({});
 });
 
 /*=========================================
@@ -94,7 +78,7 @@ gulp.task('bower', function (cb) {
 gulp.task('clean', function (cb) {
   if (config.isImage) {
     gulp.src(path.join(config.dest, 'images'), { read: false })
-       .pipe(clean({force: true}));
+       .pipe($.clean({force: true}));
   }
   return gulp.src([
         path.join(config.dest, 'index.html'),
@@ -104,7 +88,7 @@ gulp.task('clean', function (cb) {
         path.join(config.dest, 'css'),
         path.join(config.dest, 'js')
       ], { read: false })
-     .pipe(clean({force: true}));
+     .pipe($.clean({force: true}));
 });
 
 
@@ -114,7 +98,7 @@ gulp.task('clean', function (cb) {
 
 gulp.task('connect', function() {
   if (typeof config.server === 'object') {
-    connect.server({
+    $.connect.server({
       root: config.dest,
       host: config.server.host,
       port: config.server.port,
@@ -132,7 +116,7 @@ gulp.task('connect', function() {
 
 gulp.task('livereload', function () {
   gulp.src(path.join(config.dest, '*.html'))
-    .pipe(connect.reload());
+    .pipe($.connect.reload());
 });
 
 var firstInit = true;
@@ -177,12 +161,15 @@ gulp.task('html', function() {
   gulp.src('./src/images/favicon.ico')
   .pipe(gulp.dest(config.dest));
 
-  gulp.src(['./src/html/**/*.html'])
-  .pipe(replace('<!-- inject:css:before -->', injectCssBefore.join('\n    ')))
-  .pipe(replace('<!-- inject:css -->', injectCss.join('\n    ')))
-  .pipe(replace('<!-- inject:js:before -->', injectBefore.join('\n    ')))
-  .pipe(replace('<!-- inject:js -->', inject.join('\n    ')))
-  .pipe(gulp.dest(config.dest));
+  var htmlTask = gulp.src(['./src/html/**/*.html'])
+      .pipe($.replace('<!-- inject:css:before -->', injectCssBefore.join('\n    ')))
+      .pipe($.replace('<!-- inject:css -->', injectCss.join('\n    ')))
+      .pipe($.replace('<!-- inject:js:before -->', injectBefore.join('\n    ')))
+      .pipe($.replace('<!-- inject:js -->', inject.join('\n    ')));
+
+  if (!config.isDev) htmlTask.pipe(strip());
+
+  htmlTask.pipe(gulp.dest(config.dest));
 });
 
 
@@ -193,8 +180,8 @@ gulp.task('html', function() {
 gulp.task('less', function () {
   if ( firstInit ) {
     gulp.src(config.vendor.css)
-    .pipe(cssmin({keepSpecialComments : 0}))
-    .pipe(rename({
+    .pipe($.cssmin({keepSpecialComments : 0}))
+    .pipe($.rename({
       basename: "bower",
       suffix: '.min'
     }))
@@ -202,21 +189,21 @@ gulp.task('less', function () {
   }
 
   var cssTask;
-  cssTask = gulp.src(config.less.src).pipe(less({
+  cssTask = gulp.src(config.less.src).pipe($.less({
     paths: config.less.paths.map(function(p){
       return path.resolve(__dirname, p);
     })
   }))
-  .pipe(mobilizer('bootstrap.css', {
+  .pipe($.mobilizer('bootstrap.css', {
     'bootstrap.css': {
       screens: 'any'
     }
   }));
 
-  if (!config.isDev) cssTask = cssTask.pipe(cssmin({keepSpecialComments : 0}));
+  if (!config.isDev) cssTask = cssTask.pipe($.cssmin({keepSpecialComments : 0}));
 
   return cssTask
-    .pipe(rename({
+    .pipe($.rename({
       basename: "app",
       suffix: '.min'
     }))
@@ -233,33 +220,33 @@ gulp.task('js', function() {
   var jsTask;
   if ( firstInit ) {
     jsTask = gulp.src(config.vendor.js)
-    .pipe(sourcemaps.init())
-    .pipe(concat('bower.js'))
-    .pipe(ngAnnotate());
+    .pipe($.sourcemaps.init())
+    .pipe($.concat('bower.js'))
+    .pipe($.ngAnnotate());
 
-    if ( !config.isDev ) jsTask.pipe(uglify());
+    if ( !config.isDev ) jsTask.pipe($.uglify());
 
     jsTask
-    .pipe(rename({suffix: '.min'}))
-    .pipe(sourcemaps.write('.'))
+    .pipe($.rename({suffix: '.min'}))
+    .pipe($.sourcemaps.write('.'))
     .pipe(gulp.dest(path.join(config.dest, 'js', 'bower')));
   }
 
   jsTask = streamqueue(
     { objectMode: true },
     // gulp.src(config.vendor.js),
-    gulp.src('./src/js/**/*.js').pipe(ngFilesort()),
-    gulp.src(['./src/templates/cache/**/*.html']).pipe(templateCache('templates.js', { module: 'bb' }))
+    gulp.src('./src/js/**/*.js').pipe($.angularFilesort()),
+    gulp.src(['./src/templates/cache/**/*.html']).pipe($.angularTemplatecache('templates.js', { module: 'bb' }))
   )
-  .pipe(sourcemaps.init())
-  .pipe(concat('app.js'))
-  .pipe(ngAnnotate());
+  .pipe($.sourcemaps.init())
+  .pipe($.concat('app.js'))
+  .pipe($.ngAnnotate());
 
-  if (!config.isDev) jsTask.pipe(uglify());
+  if (!config.isDev) jsTask.pipe($.uglify());
 
   jsTask
-  .pipe(rename({suffix: '.min'}))
-  .pipe(sourcemaps.write('.'))
+  .pipe($.rename({suffix: '.min'}))
+  .pipe($.sourcemaps.write('.'))
   .pipe(gulp.dest(path.join(config.dest, 'js')));
 
   firstInit = false;
