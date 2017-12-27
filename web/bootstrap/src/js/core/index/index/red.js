667e14ff8e7243a7162cfbdf933be88a6f878047
skygreen2001 <skygreen2001@gmail.com>
2017-12-27 21:19:09 +0800

    tweak

--- web/bootstrap/src/js/core/index/index/red.js
+++ web/bootstrap/src/js/core/index/index/red.js
@@ -11,5 +11,66 @@ $(function(){
   // 顶部导航滚动显示底部挡板效果
   $(".navbar").addClass("nav-scroll");
 
-
+  window_width = $(window).width();
+  $(window).on('scroll',function(){
+     if(window_width > 980){
+          rubik.checkScrollForParallax();
+     }
+  });
 });
+
+var window_width;
+var rubik = {
+
+   checkScrollForParallax: debounce(function() {
+
+         no_of_elements = 0;
+         $('.parallax').each(function() {
+             var $elem = $(this);
+
+             if(isElementInViewport($elem)){
+                 var parent_top = $elem.offset().top;
+                 var window_bottom = $(window).scrollTop();
+                 var $image = $elem.find('img')
+
+               oVal = ((window_bottom - parent_top) / 3);
+                 $image.css('transform','translate3d(0px, ' + oVal + 'px, 0px)');
+             }
+           });
+
+   }, 6)
+}
+
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		clearTimeout(timeout);
+		timeout = setTimeout(function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		}, wait);
+		if (immediate && !timeout) func.apply(context, args);
+	};
+};
+
+
+function isElementInViewport(elem) {
+    var $elem = $(elem);
+
+    // Get the scroll position of the page.
+    var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+    var viewportTop = $(scrollElem).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    // Get the position of the element on the page.
+    var elemTop = Math.round( $elem.offset().top );
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+}
