27d058539795ea3829a9b37a2068c90cdf4e36cc
skygreen2001 <skygreen2001@gmail.com>
2017-12-28 21:50:38 +0800

    finish bg slow scroll works

--- web/bootstrap/www/js/index/red.js
+++ web/bootstrap/www/js/index/red.js
@@ -11,56 +11,17 @@ $(function(){
   // 顶部导航滚动显示底部挡板效果
   $(".navbar").addClass("nav-scroll");
 
-  window_width = $(window).width();
+  $.extend({"index":index});
+  var window_width = $(window).width();
   $(window).on('scroll',function(){
      if(window_width > 980){
-          rubik.checkScrollForParallax();
+        $.index.checkScrollForBgSlow();
      }
   });
 });
 
-var window_width;
-var rubik = {
-
-   checkScrollForParallax: debounce(function() {
-
-         no_of_elements = 0;
-         $('.parallax').each(function() {
-             var $elem = $(this);
-
-             if(isElementInViewport($elem)){
-                 var parent_top = $elem.offset().top;
-                 var window_bottom = $(window).scrollTop();
-                 var $image = $elem.find('img')
-
-               oVal = ((window_bottom - parent_top) / 3);
-                 $image.css('transform','translate3d(0px, ' + oVal + 'px, 0px)');
-             }
-           });
-
-   }, 6)
-}
-
-// Returns a function, that, as long as it continues to be invoked, will not
-// be triggered. The function will be called after it stops being called for
-// N milliseconds. If `immediate` is passed, trigger the function on the
-// leading edge, instead of the trailing.
-
-function debounce(func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		clearTimeout(timeout);
-		timeout = setTimeout(function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		}, wait);
-		if (immediate && !timeout) func.apply(context, args);
-	};
-};
-
-
-function isElementInViewport(elem) {
+var index = {
+  isElementInViewport: function(elem) {
     var $elem = $(elem);
 
     // Get the scroll position of the page.
@@ -73,4 +34,49 @@ function isElementInViewport(elem) {
     var elemBottom = elemTop + $elem.height();
 
     return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+  },
+  bgSlowSroll: function(){
+      no_of_elements = 0;
+      var ctrl =this;
+      $('.parallax').each(function() {
+        var $elem = $(this);
+
+        if(ctrl.isElementInViewport($elem)){
+          var parent_top = $elem.offset().top;
+          var window_bottom = $(window).scrollTop();
+          var $image = $elem.find('img')
+
+          oVal = ((window_bottom - parent_top) / 3);
+          $image.css('transform','translate3d(0px, ' + oVal + 'px, 0px)');
+        }
+      });
+  },
+  checkScrollForBgSlow: function(){
+    var timeout, wait = 6, context = this, args = arguments;
+    clearTimeout(timeout);
+    var func = this.bgSlowSroll;
+    timeout = setTimeout(function() {
+      timeout = null;
+      func.apply(context, args);
+    }, wait);
+    if (!timeout) func.apply(context, args);
+  }
+  // checkScrollForParallax : debounce(function(){this.bgSlowSroll();},6)
 }
+
+// // Returns a function, that, as long as it continues to be invoked, will not
+// // be triggered. The function will be called after it stops being called for
+// // N milliseconds. If `immediate` is passed, trigger the function on the
+// // leading edge, instead of the trailing.
+// function debounce(func, wait, immediate) {
+//   var timeout;
+//   return function() {
+//     var context = this, args = arguments;
+//     clearTimeout(timeout);
+//     timeout = setTimeout(function() {
+//       timeout = null;
+//       if (!immediate) func.apply(context, args);
+//     }, wait);
+//     if (immediate && !timeout) func.apply(context, args);
+//   };
+// }
